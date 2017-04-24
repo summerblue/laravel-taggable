b80ff5fd3bed4dca318ae8d1b85fd3f5a5e60ab7
zgldh <zgldh@hotmail.com>
2017-04-24 10:55:51 +0800

    Update Taggable.php
    Support Laravel 5.2

--- src/Taggable.php
+++ src/Taggable.php
@@ -13,8 +13,8 @@ use Illuminate\Database\Eloquent\Collection;
  */
 trait Taggable
 {
-	/** @var \EstGroupe\Taggable\Contracts\TaggingUtility **/
-	static $taggingUtility;
+    /** @var \EstGroupe\Taggable\Contracts\TaggingUtility * */
+    static $taggingUtility;
 
     /**
      * Temp storage for auto tag
@@ -32,289 +32,289 @@ trait Taggable
      */
     protected $autoTagSet = false;
 
-	/**
-	 * Boot the soft taggable trait for a model.
-	 *
-	 * @return void
-	 */
-	public static function bootTaggable()
-	{
-		if(static::untagOnDelete()) {
-			static::deleting(function($model) {
-				$model->untag();
-			});
-		}
+    /**
+     * Boot the soft taggable trait for a model.
+     *
+     * @return void
+     */
+    public static function bootTaggable()
+    {
+        if (static::untagOnDelete()) {
+            static::deleting(function ($model) {
+                $model->untag();
+            });
+        }
 
         static::saved(function ($model) {
             $model->autoTagPostSave();
         });
 
-		static::$taggingUtility = app(TaggingUtility::class);
-	}
+        static::$taggingUtility = app(TaggingUtility::class);
+    }
 
-	/**
-	 * Return collection of tagged rows related to the tagged model
-	 *
-	 * @return Illuminate\Database\Eloquent\Collection
-	 */
-	public function tagged()
-	{
-		return $this->morphMany('EstGroupe\Taggable\Model\Tagged', 'taggable')->with('tag');
-	}
+    /**
+     * Return collection of tagged rows related to the tagged model
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function tagged()
+    {
+        return $this->morphMany('EstGroupe\Taggable\Model\Tagged', 'taggable')->with('tag');
+    }
 
     public function tags()
     {
         return $this->morphToMany(static::$taggingUtility->tagModelString(), 'taggable');
     }
 
-	/**
-	 * Set the tag names via attribute, example $model->tag_names = 'foo, bar';
-	 *
-	 * @param string $value
-	 */
-	public function getTagNamesAttribute($value)
-	{
-		return implode(', ', $this->tagNames());
-	}
+    /**
+     * Set the tag names via attribute, example $model->tag_names = 'foo, bar';
+     *
+     * @param string $value
+     */
+    public function getTagNamesAttribute($value)
+    {
+        return implode(', ', $this->tagNames());
+    }
 
-	/**
-	 * Perform the action of tagging the model with the given string
-	 *
-	 * @param $tagName string or array
-	 */
-	public function tag($tagNames)
-	{
-		if(!is_array($tagNames)) {
-			$tagNames = func_get_args();
-		}
-		$tagNames = static::$taggingUtility->makeTagArray($tagNames);
+    /**
+     * Perform the action of tagging the model with the given string
+     *
+     * @param $tagName string or array
+     */
+    public function tag($tagNames)
+    {
+        if (!is_array($tagNames)) {
+            $tagNames = func_get_args();
+        }
+        $tagNames = static::$taggingUtility->makeTagArray($tagNames);
 
-		foreach($tagNames as $tagName) {
-			$this->addTag($tagName);
-		}
-	}
+        foreach ($tagNames as $tagName) {
+            $this->addTag($tagName);
+        }
+    }
 
-	/**
-	 * Return array of the tag names related to the current model
-	 *
-	 * @return array
-	 */
-	public function tagNames()
-	{
-		return $this->tags()->lists('name')->all();
-	}
+    /**
+     * Return array of the tag names related to the current model
+     *
+     * @return array
+     */
+    public function tagNames()
+    {
+        return $this->tags()->pluck('name')->all();
+    }
 
-	/**
-	 * Return array of the tag slugs related to the current model
-	 *
-	 * @return array
-	 */
-	public function tagSlugs()
-	{
-		return $this->tags()->lists('slug')->all();
-	}
+    /**
+     * Return array of the tag slugs related to the current model
+     *
+     * @return array
+     */
+    public function tagSlugs()
+    {
+        return $this->tags()->pluck('slug')->all();
+    }
 
-	/**
-	 * Remove the tag from this model
-	 *
-	 * @param $tagName string or array (or null to remove all tags)
-	 */
-	public function untag($tagNames=null)
-	{
-		if(is_null($tagNames)) {
-			$tagNames = $this->tagNames();
-		}
+    /**
+     * Remove the tag from this model
+     *
+     * @param $tagName string or array (or null to remove all tags)
+     */
+    public function untag($tagNames = null)
+    {
+        if (is_null($tagNames)) {
+            $tagNames = $this->tagNames();
+        }
 
-		$tagNames = static::$taggingUtility->makeTagArray($tagNames);
+        $tagNames = static::$taggingUtility->makeTagArray($tagNames);
 
-		foreach($tagNames as $tagName) {
-			$this->removeTag($tagName);
-		}
+        foreach ($tagNames as $tagName) {
+            $this->removeTag($tagName);
+        }
 
-		if(static::shouldDeleteUnused()) {
-			static::$taggingUtility->deleteUnusedTags();
-		}
-	}
+        if (static::shouldDeleteUnused()) {
+            static::$taggingUtility->deleteUnusedTags();
+        }
+    }
 
-	/**
-	 * Replace the tags from this model
-	 *
-	 * @param $tagName string or array
-	 */
-	public function retag($tagNames)
-	{
-		if(!is_array($tagNames)) {
-			$tagNames = func_get_args();
-		}
-		$tagNames = static::$taggingUtility->makeTagArray($tagNames);
-		$currentTagNames = $this->tagNames();
+    /**
+     * Replace the tags from this model
+     *
+     * @param $tagName string or array
+     */
+    public function retag($tagNames)
+    {
+        if (!is_array($tagNames)) {
+            $tagNames = func_get_args();
+        }
+        $tagNames = static::$taggingUtility->makeTagArray($tagNames);
+        $currentTagNames = $this->tagNames();
 
-		$deletions = array_diff($currentTagNames, $tagNames);
-		$additions = array_diff($tagNames, $currentTagNames);
+        $deletions = array_diff($currentTagNames, $tagNames);
+        $additions = array_diff($tagNames, $currentTagNames);
 
-		$this->untag($deletions);
+        $this->untag($deletions);
 
-		foreach($additions as $tagName) {
-			$this->addTag($tagName);
-		}
-	}
+        foreach ($additions as $tagName) {
+            $this->addTag($tagName);
+        }
+    }
 
-	/**
-	 * Filter model to subset with the given tags
-	 *
-	 * @param $tagNames array|string
-	 */
-	public function scopeWithAllTags($query, $tagNames)
-	{
-		if(!is_array($tagNames)) {
-			$tagNames = func_get_args();
-			array_shift($tagNames);
-		}
-		$tagNames = static::$taggingUtility->makeTagArray($tagNames);
+    /**
+     * Filter model to subset with the given tags
+     *
+     * @param $tagNames array|string
+     */
+    public function scopeWithAllTags($query, $tagNames)
+    {
+        if (!is_array($tagNames)) {
+            $tagNames = func_get_args();
+            array_shift($tagNames);
+        }
+        $tagNames = static::$taggingUtility->makeTagArray($tagNames);
 
-		$model = static::$taggingUtility->tagModelString();
-		$tagids = $model::byTagNames($tagNames)->lists('id')->all();
+        $model = static::$taggingUtility->tagModelString();
+        $tagids = $model::byTagNames($tagNames)->pluck('id')->all();
 
-		$className = $query->getModel()->getMorphClass();
-		$primaryKey = $this->getKeyName();
+        $className = $query->getModel()->getMorphClass();
+        $primaryKey = $this->getKeyName();
 
-		$tagid_count = count($tagids);
-		if ($tagid_count > 0) {
-			$ids = Tagged::where('taggable_type', $className)
-							->whereIn('tag_id', $tagids)
-							->whereRaw('`tag_id` in (' .implode(',', $tagids). ') group by taggable_id having count(taggable_id) ='.$tagid_count)
-							->lists('taggable_id');
+        $tagid_count = count($tagids);
+        if ($tagid_count > 0) {
+            $ids = Tagged::where('taggable_type', $className)
+                ->whereIn('tag_id', $tagids)
+                ->whereRaw('`tag_id` in (' . implode(',', $tagids) . ') group by taggable_id having count(taggable_id) =' . $tagid_count)
+                ->pluck('taggable_id');
 
-			$query->whereIn($this->getTable().'.'.$primaryKey, $ids);
-		}
+            $query->whereIn($this->getTable() . '.' . $primaryKey, $ids);
+        }
 
-		return $query;
-	}
+        return $query;
+    }
 
-	/**
-	 * Filter model to subset with the given tags
-	 *
-	 * @param $tagNames array|string
-	 */
-	public function scopeWithAnyTag($query, $tagNames)
-	{
-		if(!is_array($tagNames)) {
-			$tagNames = func_get_args();
-			array_shift($tagNames);
-		}
-		$tagNames = static::$taggingUtility->makeTagArray($tagNames);
+    /**
+     * Filter model to subset with the given tags
+     *
+     * @param $tagNames array|string
+     */
+    public function scopeWithAnyTag($query, $tagNames)
+    {
+        if (!is_array($tagNames)) {
+            $tagNames = func_get_args();
+            array_shift($tagNames);
+        }
+        $tagNames = static::$taggingUtility->makeTagArray($tagNames);
 
-		$model = static::$taggingUtility->tagModelString();
-		$tagids = $model::byTagNames($tagNames)->lists('id')->all();
+        $model = static::$taggingUtility->tagModelString();
+        $tagids = $model::byTagNames($tagNames)->pluck('id')->all();
 
-		$className = $query->getModel()->getMorphClass();
-		$primaryKey = $this->getKeyName();
+        $className = $query->getModel()->getMorphClass();
+        $primaryKey = $this->getKeyName();
 
-		$tags = Tagged::whereIn('tag_id', $tagids)
-			->where('taggable_type', $className)
-			->lists('taggable_id');
+        $tags = Tagged::whereIn('tag_id', $tagids)
+            ->where('taggable_type', $className)
+            ->pluck('taggable_id');
 
-		return $query->whereIn($this->getTable().'.'.$primaryKey, $tags);
-	}
+        return $query->whereIn($this->getTable() . '.' . $primaryKey, $tags);
+    }
 
-	/**
-	 * Adds a single tag
-	 *
-	 * @param $tagName string
-	 */
-	private function addTag($tagName)
-	{
-		$model = static::$taggingUtility->tagModelString();
-		$tag = $model::byTagName($tagName)->first();
+    /**
+     * Adds a single tag
+     *
+     * @param $tagName string
+     */
+    private function addTag($tagName)
+    {
+        $model = static::$taggingUtility->tagModelString();
+        $tag = $model::byTagName($tagName)->first();
 
-		if($tag) {
-			// If tag is exists, do not create
-			$count = $this->tagged()->where('tag_id', '=', $tag->id)->take(1)->count();
-			if($count >= 1) {
-				return;
-			} else {
-				$this->tags()->attach($tag->id);
-			}
-		} else {
-			// If tag is not exists, create tag and attach to object
-			$tag = new $model;
-			$tag->name = $tagName;
-			$tag->save();
+        if ($tag) {
+            // If tag is exists, do not create
+            $count = $this->tagged()->where('tag_id', '=', $tag->id)->take(1)->count();
+            if ($count >= 1) {
+                return;
+            } else {
+                $this->tags()->attach($tag->id);
+            }
+        } else {
+            // If tag is not exists, create tag and attach to object
+            $tag = new $model;
+            $tag->name = $tagName;
+            $tag->save();
 
-			$this->tags()->attach($tag->id);
-		}
-		static::$taggingUtility->incrementCount($tag, 1);
+            $this->tags()->attach($tag->id);
+        }
+        static::$taggingUtility->incrementCount($tag, 1);
 
-		if (config('taggable.is_tagged_label_enable')
-			&& $this->is_tagged != 'yes'
-		) {
-			$this->is_tagged = 'yes';
-			$this->save();
-		}
+        if (config('taggable.is_tagged_label_enable')
+            && $this->is_tagged != 'yes'
+        ) {
+            $this->is_tagged = 'yes';
+            $this->save();
+        }
 
-		unset($this->relations['tagged']);
-		event(new TagAdded($this));
-	}
+        unset($this->relations['tagged']);
+        event(new TagAdded($this));
+    }
 
-	/**
-	 * Removes a single tag
-	 *
-	 * @param $tagName string
-	 */
-	private function removeTag($tagName)
-	{
-		$tag = $this->tags()->byTagName($tagName)->first();
+    /**
+     * Removes a single tag
+     *
+     * @param $tagName string
+     */
+    private function removeTag($tagName)
+    {
+        $tag = $this->tags()->byTagName($tagName)->first();
 
-		if ($tag) {
-			$this->tags()->detach($tag->id);
-			static::$taggingUtility->decrementCount($tag, 1);
-		}
+        if ($tag) {
+            $this->tags()->detach($tag->id);
+            static::$taggingUtility->decrementCount($tag, 1);
+        }
 
-		if (config('taggable.is_tagged_label_enable')
-			&& $this->is_tagged != 'no'
-			&& $this->tags()->count() <= 0
-		) {
-			$this->is_tagged = 'no';
-			$this->save();
-		}
+        if (config('taggable.is_tagged_label_enable')
+            && $this->is_tagged != 'no'
+            && $this->tags()->count() <= 0
+        ) {
+            $this->is_tagged = 'no';
+            $this->save();
+        }
 
-		unset($this->relations['tagged']);
-		event(new TagRemoved($this));
-	}
+        unset($this->relations['tagged']);
+        event(new TagRemoved($this));
+    }
 
-	/**
-	 * Return an array of all of the tags that are in use by this model
-	 *
-	 * @return Collection
-	 */
-	public static function existingTags()
-	{
-		$tags_table_name = config('taggable.tags_table_name');
+    /**
+     * Return an array of all of the tags that are in use by this model
+     *
+     * @return Collection
+     */
+    public static function existingTags()
+    {
+        $tags_table_name = config('taggable.tags_table_name');
 
-		return Tagged::distinct()
-			->join($tags_table_name, 'tag_id', '=', $tags_table_name.'.id')
-			->where('taggable_type', '=', (new static)->getMorphClass())
-			->orderBy('tag_id', 'ASC')
-			->get(array($tags_table_name.'.slug as slug', $tags_table_name.'.name as name', $tags_table_name.'.count as count'));
-	}
+        return Tagged::distinct()
+            ->join($tags_table_name, 'tag_id', '=', $tags_table_name . '.id')
+            ->where('taggable_type', '=', (new static)->getMorphClass())
+            ->orderBy('tag_id', 'ASC')
+            ->get(array($tags_table_name . '.slug as slug', $tags_table_name . '.name as name', $tags_table_name . '.count as count'));
+    }
 
-	/**
-	 * Should untag on delete
-	 */
-	public static function untagOnDelete()
-	{
-		return isset(static::$untagOnDelete)
-			? static::$untagOnDelete
-			: config('taggable.untag_on_delete');
-	}
+    /**
+     * Should untag on delete
+     */
+    public static function untagOnDelete()
+    {
+        return isset(static::$untagOnDelete)
+            ? static::$untagOnDelete
+            : config('taggable.untag_on_delete');
+    }
 
-	/**
-	 * Delete tags that are not used anymore
-	 */
-	public static function shouldDeleteUnused()
-	{
-		return config('taggable.delete_unused_tags');
-	}
+    /**
+     * Delete tags that are not used anymore
+     */
+    public static function shouldDeleteUnused()
+    {
+        return config('taggable.delete_unused_tags');
+    }
 
     /**
      * Set tag names to be set on save
@@ -352,22 +352,22 @@ trait Taggable
         }
     }
 
-	/**
-	 * by @CJ
-	 * Sync tags with tag_id array
-	 *
-	 * @param $tag_ids tag_id array
-	 */
-	public function tagWithTagIds($tag_ids = [])
+    /**
+     * by @CJ
+     * Sync tags with tag_id array
+     *
+     * @param $tag_ids tag_id array
+     */
+    public function tagWithTagIds($tag_ids = [])
     {
         if (count($tag_ids) <= 0) {
             return;
         }
 
-		$model = static::$taggingUtility->tagModelString();
-		$tag_names = $model::byTagIds($tag_ids)->lists('name')->all();
+        $model = static::$taggingUtility->tagModelString();
+        $tag_names = $model::byTagIds($tag_ids)->pluck('name')->all();
 
-		$this->retag($tag_names);
+        $this->retag($tag_names);
     }
 
 
