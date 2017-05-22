cd64468463586fb89b0d947a1a3acc6812d25af6
zgldh <zgldh@hotmail.com>
2017-05-22 19:02:40 +0800

    fix typo

--- src/Taggable.php
+++ src/Taggable.php
@@ -358,3 +358,26 @@ trait Taggable
             } else {
                 $this->untag();
             }
+        }
+    }
+
+    /**
+     * by @CJ
+     * Sync tags with tag_id array
+     *
+     * @param $tag_ids tag_id array
+     */
+    public function tagWithTagIds($tag_ids = [])
+    {
+        if (count($tag_ids) <= 0) {
+            return;
+        }
+
+        $model = static::$taggingUtility->tagModelString();
+        $tag_names = $model::byTagIds($tag_ids)->pluck('name')->all();
+
+        $this->retag($tag_names);
+    }
+
+
+}
